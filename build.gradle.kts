e03d1ae2acbdc1f991255aae710f3c8f74124e63
Dmitry Schitinin <dmitry.schitinin@corp.mail.ru>
2019-09-25 11:41:34 +0300

    Build and CI stuff added

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+// Apply the java plugin to add support for Java
+plugins {
+    java
+    application
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    // Guava primitives
+    compile("com.google.guava:guava:27.0.1-jre")
+
+    // JUnit Jupiter test framework
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.4.0")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.0")
+}
+
+val run by tasks.getting(JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks {
+    test {
+        maxHeapSize = "128m"
+        useJUnitPlatform()
+    }
+}
+
