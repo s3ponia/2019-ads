6b7a3998aff8aa58af2c99a490a22c66f2899428
s3ponia <slava.shlyaga@gmail.com>
2019-10-20 22:20:17 +0300

    fix mergesort

--- src/main/java/ru/mail/polis/ads/part3/s3ponia/MergeSort.java
+++ src/main/java/ru/mail/polis/ads/part3/s3ponia/MergeSort.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 
 /**
- * https://www.e-olymp.com/ru/submissions/5891319
+ * https://www.e-olymp.com/ru/submissions/5908069
  */
 public class MergeSort {
 
@@ -23,6 +23,42 @@ public class MergeSort {
             return 0;
         }
         return p1.firstValue - p2.firstValue > 0 ? 1 : -1;
+    }
+
+    private static void merge(Pair[] input, int start, int med, int e) {
+        Pair[] res = new Pair[e - start];
+
+        int j = 0;
+        int it1 = start;
+        int it2 = med;
+
+        while (it1 < med && it2 < e) {
+            if (comparator(input[it1], input[it2]) <= 0) {
+                res[j++] = input[it1++];
+            } else {
+                res[j++] = input[it2++];
+            }
+        }
+
+        while (it1 < med) {
+            res[j++] = input[it1++];
+        }
+
+        while (it2 < e) {
+            res[j++] = input[it2++];
+        }
+
+        System.arraycopy(res, 0, input, start, res.length);
+
+    }
+
+    private static void sort(Pair[] arr, int b, int e) {
+        if ((e - b) <= 1)
+            return;
+
+        sort(arr, b, (e + b) / 2);
+        sort(arr, (e + b) / 2, e);
+        merge(arr, b, (e + b) / 2, e);
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,7 +76,7 @@ public class MergeSort {
             robots[i] = input;
         }
 
-        Arrays.sort(robots, MergeSort::comparator);
+        sort(robots, 0, robots.length);
 
         StringBuilder stringBuilder = new StringBuilder(20);
         for (Pair pair :
