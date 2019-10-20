00f253b0959731c69fdc21b56a9ffef3e4b5aac4
s3ponia <slava.shlyaga@gmail.com>
2019-10-20 22:12:31 +0300

    replace Array.sort 1st task

--- src/main/java/ru/mail/polis/ads/part3/s3ponia/KthStatistics.java
+++ src/main/java/ru/mail/polis/ads/part3/s3ponia/KthStatistics.java
@@ -7,9 +7,45 @@ import java.math.BigInteger;
 import java.util.Arrays;
 
 /**
- * https://www.e-olymp.com/ru/submissions/5890221
+ * https://www.e-olymp.com/ru/submissions/5907983
  */
 public class KthStatistics {
+
+    private static void merge(BigInteger[] input, int start, int med, int e) {
+        BigInteger[] res = new BigInteger[e - start];
+
+        int j = 0;
+        int it1 = start;
+        int it2 = med;
+
+        while (it1 < med && it2 < e) {
+            if (input[it1].compareTo(input[it2]) < 0) {
+                res[j++] = input[it1++];
+            } else {
+                res[j++] = input[it2++];
+            }
+        }
+
+        while (it1 < med) {
+            res[j++] = input[it1++];
+        }
+
+        while (it2 < e) {
+            res[j++] = input[it2++];
+        }
+
+        System.arraycopy(res, 0, input, start, res.length);
+
+    }
+
+    private static void sort(BigInteger[] arr, int b, int e) {
+        if ((e - b) <= 1)
+            return;
+
+        sort(arr, b, (e + b) / 2);
+        sort(arr, (e + b) / 2, e);
+        merge(arr, b, (e + b) / 2, e);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +57,7 @@ public class KthStatistics {
             array[i] = new BigInteger(ints[i]);
         }
 
-        Arrays.sort(array);
+        sort(array, 0, array.length);
 
         System.out.println(array[array.length - k]);
     }
