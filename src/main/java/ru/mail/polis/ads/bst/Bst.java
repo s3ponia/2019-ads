9865f540c02c8950d47e932399aaefdc4e105c81
Dmitry Schitinin <dmitry.schitinin@corp.mail.ru>
2019-10-29 01:58:14 +0300

    AVL BST task

---
+++ src/main/java/ru/mail/polis/ads/bst/Bst.java
@@ -0,0 +1,28 @@
+package ru.mail.polis.ads.bst;
+
+/**
+ * Binary search tree with ordered operations support.
+ */
+public interface Bst<Key extends Comparable<Key>, Value> {
+    Value get(Key key);
+
+    void put(Key key, Value value);
+
+    void remove(Key key);
+
+    Key min();
+
+    Value minValue();
+
+    Key max();
+
+    Value maxValue();
+
+    Key floor(Key key);
+
+    Key ceil(Key key);
+    
+    int size();
+    
+    int height();
+}
