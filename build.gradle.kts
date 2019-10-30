463fc7129d85aa2de49b0cbfed02f75137efd50c
BogdanMendli <45788828+BogdanMendli@users.noreply.github.com>
2019-10-30 16:38:18 +0300

    added dependency (#182)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ dependencies {
     // Guava primitives
     compile("com.google.guava:guava:27.0.1-jre")
 
+    // Annotation
+    implementation ("org.jetbrains:annotations:16.0.3")
+
     // JUnit Jupiter test framework
     testCompile("org.junit.jupiter:junit-jupiter-api:5.4.0")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.0")
